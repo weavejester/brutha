b14727c1de87d78bbf704259b372cb4dd5d506d5
James Reeves <jreeves@weavejester.com>
2015-05-05 16:45:33 +0100

    Add description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject brutha "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple ClojureScript interface to React"
+  :url "https://github.com/weavejester/brutha"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]

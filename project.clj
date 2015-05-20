297c74bfd077ab5e853d13b3375adea1e5c5e3c8
James Reeves <jreeves@weavejester.com>
2015-05-20 18:23:14 +0100

    Update React dependency to 0.13.3

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-beta3"]
                  [org.clojure/clojurescript "0.0-3269" :scope "provided"]
-                 [cljsjs/react "0.13.1-0"]]
+                 [cljsjs/react "0.13.3-0"]]
   :plugins [[lein-cljsbuild "1.0.5"]]
   :cljsbuild
   {:test-commands {"unit-tests" ["phantomjs" :runner "target/main.js"]}

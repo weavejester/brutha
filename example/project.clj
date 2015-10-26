aedb699a90ea54a6b0591c431e0ea191ff8de896
James Reeves <jreeves@weavejester.com>
2015-10-26 05:34:39 +0000

    Update Flupot dependency in example project

--- example/project.clj
+++ example/project.clj
@@ -5,10 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.122"]
-                 [cljsjs/react "0.14.0-0"]
-                 [cljsjs/react-dom "0.14.0-0"]
                  [brutha "0.1.5"]
-                 [flupot "0.1.1"]]
+                 [flupot "0.2.0"]]
   :plugins [[lein-cljsbuild "1.1.0"]]
   :cljsbuild
   {:builds {:main {:source-paths ["src"]
